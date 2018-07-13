6678813c7fd9b21052f85f5063b13f7962061c0d
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-13 11:10:26 +0100

    first commit

---
+++ db/cinema_db.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets;
+DROP TABLE films;
+DROP TABLE customers;
+
+CREATE TABLE  customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds VARCHAR(255)
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE tickets(
+  id VARCHAR(255),
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  movie_id INT4 REFERENCES movies(id) ON DELETE CASCADE
+);
